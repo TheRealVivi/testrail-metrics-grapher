560c5a65fec38634cf5d1f271787acd2998d7eb7
ODellKyle <48389758+ODellKyle@users.noreply.github.com>
2022-11-08 11:31:59 -0800

    Added dynamic graphing with arrays

--- test/LaunchMenu.py
+++ test/LaunchMenu.py
@@ -7,8 +7,11 @@ import subprocess
 
 
 def MainMenu():
+    from dash import Dash, html, dcc
+    app = Dash(__name__)
+
     toolActive = True
-    
+    figs = []
     while toolActive:
         option = input("Welcome to the TestRail Metrics Generator Tool!\n"
                      + "Please select an option:\n"
@@ -20,7 +23,7 @@ def MainMenu():
         
         if option == '1':
             subprocess.run(["java", "-jar", "../vMetrics.jar", "1"])
-            PieGraphConsolidatedMetrics()
+            figs.append(PieGraphConsolidatedMetrics())
         elif option == '2':
             subprocess.run(["java", "-jar", "../vMetrics.jar", "2"])
         elif option == '3':
@@ -31,16 +34,43 @@ def MainMenu():
             print("\nInvalid input! Please try again.\n")
             
         print()
-        
+
     print("\nGoodbye\n")
+
+    graphs = []
+    for i in range(0, len(figs)):
+        graphs.append(html.Div(dcc.Graph(
+            id='graph-{}'.format(i),
+            figure=figs[i]
+        )))
+
+    if(len(graphs) != 0):
+        app.layout = html.Div(children=[
+            html.H1(children='Hello Dash'),
+
+            html.Div(children='''
+                Dash: A web app framework for your data.
+            '''),
+
+
+            *graphs
+            
+        ])
+        
+        app.run_server(debug=False)
     
+
+# Adds pie chart to subplot
 def PieGraphConsolidatedMetrics():
     import plotly.express as px
     import pandas as pd
+
     df = pd.read_csv('../../../Documents/consolidatedMetrics.csv')
     df = df.drop(labels=[7], axis=0,)
     fig = px.pie(df, values=' Test Count', names='Test Case Status')
-    fig.write_html('TestCaseStatus_PieFigure.html', auto_open=True)
+    
+    return fig
+    
 
 if __name__ == '__main__':
     MainMenu()
