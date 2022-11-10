ce3a391bcf96ee3e1220313c1b1861ec9fad96fb
ODellKyle <48389758+ODellKyle@users.noreply.github.com>
2022-11-10 14:26:43 -0800

    Updated Java program; user now only has to login once.
    Credentials are provided through python, and passed to java program

--- driver/main.py
+++ driver/main.py
@@ -4,9 +4,10 @@ Created on Nov 4, 2022
 @author: Kyle O'Dell
 '''
 
-from menus import MainMenu
+from menus import MainMenu, Login
 from testRailMetricsGraphingTool import LaunchDashApp
 
 if __name__ == '__main__':
-    figs = MainMenu()
+    (domain, email, password) = Login()
+    figs = MainMenu(domain, email, password)
     LaunchDashApp(figs)
--- driver/menus.py
+++ driver/menus.py
@@ -1,24 +1,37 @@
-def MainMenu():
+def Login() -> str:
+    print("Please login to TestRail:")
+    domain:str = input("TestRail domain: ")
+    email:str = input("TestRail email: ")
+    password:str = input("TestRail password: ")
+
+    return (domain, email, password)
+
+def MainMenu(domain:str, email:str, password:str):
     import subprocess
 
     toolActive = True
+    consolidatedMetricsPulledRecently = False
+
     figs = []
     while toolActive:
-        option = input("Welcome to the TestRail Metrics Generator Tool!\n"
+        option = input("\nWelcome to the TestRail Metrics Generator Tool!\n"
                      + "Please select an option:\n"
-                     + "1. Print consolidated section metrics\n"
-                     + "2. Print metrics per section\n"
-                     + "3. Print test run metrics\n"
+                     + "1. Provide consolidated section metrics\n"
+                     + "2. Provide metrics per section\n"
+                     + "3. Provide test run metrics\n"
                      + "4. Quit\n"
                      + "Input option (1-4) >> ")
         
         if option == '1':
-            subprocess.run(["java", "-jar", "../vMetrics.jar", "1"])
+            if not consolidatedMetricsPulledRecently:
+                subprocess.run(["java", "-jar", "../vMetrics.jar", "1", domain, email, password])
+                consolidatedMetricsPulledRecently = True
+            
             figs.append(ConsolidatedMetricsGraphOptionsMenu())
         elif option == '2':
-            subprocess.run(["java", "-jar", "../vMetrics.jar", "2"])
+            subprocess.run(["java", "-jar", "../vMetrics.jar", "2", domain, email, password])
         elif option == '3':
-            subprocess.run(["java", "-jar", "../vMetrics.jar", "3"])
+            subprocess.run(["java", "-jar", "../vMetrics.jar", "3", domain, email, password])
         elif option == '4':
             toolActive = False
         else:
@@ -35,7 +48,7 @@ def ConsolidatedMetricsGraphOptionsMenu():
     from testRailMetricsGraphingTool import PieGraphConsolidatedMetrics
     from testRailMetricsGraphingTool import BarGraphConsolidatedMetrics
 
-    option = input("Select chart type to plot metrics:\n"
+    option = input("\nSelect chart type to plot metrics:\n"
                  + "1. Pie chart\n"
                  + "2. Bar graph\n"
                  + "Input option >> ")
@@ -48,4 +61,5 @@ def ConsolidatedMetricsGraphOptionsMenu():
         print("No valid option selected; defaulting to pie chart")
         fig = PieGraphConsolidatedMetrics()
 
+    print("\nCreated and appended graph.\n")
     return fig
